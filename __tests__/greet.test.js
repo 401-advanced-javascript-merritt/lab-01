3cc7137ebba9ad770d759b3753cb2b463f3d5eba
christopherknightmerritt <chris.merritt95@gmail.com>
2019-03-26 09:08:00 -0700

    fixed linter

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -3,27 +3,27 @@
 const greet = require('../lib/greet.js');
 
 describe ('greet.js', ()=> {
-    it('Int should be null', ()=> {
-        expect(greet(1)).toBeNull();
-    });
-    it('Arr should be null', ()=> {
-        expect(greet([])).toBeNull();
-    });
-    it('Obj should be null', ()=> {
-        expect(greet({})).toBeNull();
-    });
-    it('True should be null', ()=> {
-        expect(greet(true)).toBeNull();
-    });
-    it('False should be null', ()=> {
-        expect(greet(false)).toBeNull();
-    });
-    it('Undefined should be null', ()=> {
-        expect(greet(undefined)).toBeNull();
-    });
+  it('Int should be null', ()=> {
+    expect(greet(1)).toBeNull();
+  });
+  it('Arr should be null', ()=> {
+    expect(greet([])).toBeNull();
+  });
+  it('Obj should be null', ()=> {
+    expect(greet({})).toBeNull();
+  });
+  it('True should be null', ()=> {
+    expect(greet(true)).toBeNull();
+  });
+  it('False should be null', ()=> {
+    expect(greet(false)).toBeNull();
+  });
+  it('Undefined should be null', ()=> {
+    expect(greet(undefined)).toBeNull();
+  });
 
-    it('should say hello world', () =>{
-        let res = greet('world');
-        expect(res).toEqual('Hello world');
-    });
+  it('should say hello world', () =>{
+    let res = greet('world');
+    expect(res).toEqual('Hello world');
+  });
 });
